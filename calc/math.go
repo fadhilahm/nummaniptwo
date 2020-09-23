ff22dc0602536259eb602861b1046b3ecd55daaf
fadhilahm <fadhilahmetra@gmail.com>
2020-09-23 23:40:32 +0700

    Calc.Add now accept variadic function argument

--- calc/math.go
+++ calc/math.go
@@ -1,6 +1,12 @@
 package calc
 
 // Add function will run addition process
-func Add (i int, j int) int {
-	return i + j
+func Add (numbers ...int) int {
+	sum := 0
+
+	for _, num:= range numbers {
+		sum = sum + num
+	}
+
+	return sum
 }
